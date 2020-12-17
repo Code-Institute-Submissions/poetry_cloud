c68238ae0b4815add18e24580503828d436f5236
JohnW876 <john@illumino.co.uk>
2020-12-17 19:52:31 +0000

    Added registration functionality

--- app.py
+++ app.py
@@ -17,17 +17,38 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-# Here, the @app.route decorator is used to bind the URL to the get_poems function.
-# As a result, when the user visits the URL, the output of the function will render in the browser.
+# Here, the @app.route decorator is used to bind the URL to the get_poems
+# function.
+# As a result, when the user visits the URL, the output of the function will
+# render in the browser.
 @app.route("/")
 @app.route("/get_poems")
 def get_poems():
     poems = mongo.db.poems.find()
     return render_template("poems.html", poems=poems)
 
-
+# Register function
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Verify if the username already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("That username is already in use, please choose another.")
+            return redirect(url_for("register"))
+        # Gets the details from the register form
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        # Inserts the user into the users collection
+        mongo.db.users.insert_one(register)
+
+        # Place the newly created user into session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("You have successfully registered!")
     return render_template("register.html")
 
 
