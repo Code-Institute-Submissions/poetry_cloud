5aa1f9ddf9fe565c4d66eda9142d6b241d5bd9e6
JohnW876 <john@illumino.co.uk>
2020-12-17 15:05:59 +0000

    Styled colours in navbar and added register form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -16,7 +17,8 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
+# Here, the @app.route decorator is used to bind the URL to the get_poems function.
+# As a result, when the user visits the URL, the output of the function will render in the browser.
 @app.route("/")
 @app.route("/get_poems")
 def get_poems():
@@ -24,6 +26,12 @@ def get_poems():
     return render_template("poems.html", poems=poems)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+# How and where to run the application.
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
